63ae2cbfc2abf387afd0fbafc9bdcc11eb3b8dda
yongtae <gyt08301@naver.com>
2023-12-27 17:31:42 +0900

    yongtae

--- components/component/yongtae.tsx
+++ components/component/yongtae.tsx
@@ -1,12 +1,13 @@
 // components/BookDetailComponent.js 또는 .tsx
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { Button } from "@/components/ui/button";
 
 const Yongtae = ({ bookId, setSelectedCompoId }) => {
     const [book, setBook] = useState(null);
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
-
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
     useEffect(() => {
         if (bookId) {
             axios.get(`http://127.0.0.1:8000/api/BookList/${bookId}`)
@@ -31,24 +32,79 @@ const Yongtae = ({ bookId, setSelectedCompoId }) => {
         }
     };
 
+    const handleSubmit = () => {
+        console.log(title, content);
+        // 여기서 서버로 데이터를 전송하는 로직을 구현합니다.
+    };
+    const [isContentFocused, setIsContentFocused] = useState(false);
+    const [image, setImage] = useState(null);
+    const textareaRef = useRef(null);
+    
+
     return (
         <div>
             {book ? (
-                <div>
-                    <h1>{book.book_name}</h1>
-                    <p>저자: {book.author}</p>
-                    <p>장르: {book.genre}</p>
-                    <p>난이도: {book.level}</p>
-                    {book.BookDetail && book.BookDetail.length > 0 && (
-                        <p>내용: {book.BookDetail[currentPageIndex].content}</p>
-                    )}
-                    <Button onClick={goToPreviousPage}>이전 페이지</Button>
-                    <Button onClick={goToNextPage}>다음 페이지</Button>
-                    <Button onClick={() => {
-                        setSelectedCompoId(null);
-                        
-                    }}>뒤로가기</Button>
-                </div>
+                <>
+                    <button class="bg-green hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" style={{ marginBottom: '1rem'}}>목록</button>
+                    <button class="bg-green hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" style={{ marginLeft: '1rem'}}>뒤로가기</button>   
+                    <div style={{ marginBottom: '1rem', display: 'flex', justifyContent: 'center' }}>
+                        <input
+                            type="text"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            placeholder="제목을 입력하세요."
+                            style={{
+                                width: '100%',
+                                borderRadius: '10px',
+                                border: '3px solid #ccc',
+                                padding: '8px',
+                            }}
+                        />
+                    </div>
+                    <div style={{ marginBottom: '1rem', display: 'flex', justifyContent: 'center' }}>
+                        <textarea
+                            ref={textareaRef}
+                            value={content}
+                            onClick={() => textareaRef.current.focus()}
+                            onChange={(e) => setContent(e.target.value)}
+                            placeholder="본문을 입력하세요."
+                            rows="20"
+                            style={{
+                                width: '100%',
+                                borderRadius: '10px',
+                                border: '3px solid #ccc',
+                                padding: '8px',
+                            }}
+                        />
+                    </div>
+                    <div style={{ marginBottom: '1rem', display: 'flex', justifyContent: 'space-between' }}>
+                        <div>
+                            <input
+                                type="file"
+                                onChange={(e) => {
+                                    const file = e.target.files[0];
+                                    const reader = new FileReader();
+                                    reader.onload = (event) => {
+                                        setImage(event.target.result);
+                                    };
+                                    reader.readAsDataURL(file);
+                                }}
+                            />
+                        </div>
+                        <div>
+                            <button
+                                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                등록하기
+                            </button>
+                        </div>
+                    </div>
+                    {/* {image && (
+                        <div style={{ marginBottom: '1rem', display: 'flex', justifyContent: 'center' }}>
+                            <img src={image} alt="첨부 이미지" style={{ maxWidth: '100%', height: 'auto' }} />
+                        </div>
+                    )} */}
+
+                </>
             ) : (
                 <div>Loading...</div>
             )}
