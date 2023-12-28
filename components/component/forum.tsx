b2e10e04fc8479ed2ed5d71ad96b487413ba890e
inha <qaqrs99@gmail.com>
2023-12-28 15:12:32 +0900

    게시물상세조회화면

--- components/component/forum.tsx
+++ components/component/forum.tsx
@@ -3,24 +3,29 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
-const Forum = ({ postId, goBack }) => {
+const Forum = ({ bookId, setSelectedCompoId }) => {
     const [book, setBook] = useState(null);
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
 
 
+    //수정
+    const [comment, setcomment] =useState("")
+
     return (
         <div className="container mx-auto px-4">
         {/* 뒤로가기 버튼 */}
-        <button className="bg-white-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mb-4" onClick={goBack}>
-            <img src='https://kr.seaicons.com/wp-content/uploads/2015/11/Arrows-Undo-icon.png' className='w-8 h-8'/>
-            
+        <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mb-4">
+            뒤로가기
         </button>
 
         {/* 게시물 내용 */}
-        <div className="bg-white p-4 rounded-lg shadow-md mb-6">
+        <div className="bg-white p-6 rounded-lg shadow-md mb-6">
             <h1 className="text-3xl font-bold mb-4">게시물 제목</h1>
-            <p className="text-gray-700 mb-2">작성자: 홍길동</p>
-            <p className="text-gray-700 mb-4">작성 시간: 2023-12-27</p>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <p className="text-gray-700 mb-2 text-lg font-bold"> 홍길동</p>
+            <p className="text-gray-700 mb-4 text-lg font-bold"> 2023-12-27</p>
+            </div>
+            <div><br></br></div>
             <div className="text-gray-800">
                 {/* 게시물 본문 내용 */}
                 본문 즉 질문이 들어갈 자리입니다...
@@ -28,12 +33,13 @@ const Forum = ({ postId, goBack }) => {
         </div>
 
         {/* 댓글 테이블 */}
-        <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-6 rounded-lg shadow-md mb-6">
             <table className="min-w-full">
+                <caption className='text-left mb-4 text-gray-700'>댓글</caption>
                 <thead className="bg-gray-100">
                     <tr>
-                        <th className="border-b-2 border-gray-200 p-4 text-center">댓글 작성자</th>
-                        <th className="border-b-2 border-gray-200 p-4 text-center">댓글 내용</th>
+                        <th className="border-b-2 border-gray-200 p-4 text-center">작성자</th>
+                        <th className="border-b-2 border-gray-200 p-4 text-center">내용</th>
                         <th className="border-b-2 border-gray-200 p-4 text-center">작성일자</th>
                     </tr>
                 </thead>
@@ -46,16 +52,16 @@ const Forum = ({ postId, goBack }) => {
                     {/* 추가적인 댓글 행들을 필요에 따라 반복해서 추가 */}
                 </tbody>
             </table>
-        </div>
-
+            <br></br>
+            <input id='new_comment' type='text' value={comment} onChange={(e) =>  setcomment(e.target.value)} className='w-5/6 mb-2 p-2 border border-gray-300' required placeholder='댓글 내용'/>
         {/* 댓글 작성하기 버튼 */}
-        <div className="text-right my-6">
-            <button className="bg-Stone-50 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
-                <img src='https://cdn.icon-icons.com/icons2/2066/PNG/512/comments_icon_125320.png' className='w-8 h-8' />
-                
+            <button className="ml-20 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+                작성하기
             </button>
         </div>
-    </div>
+        </div>
+
+        
 );
 };
 
