c03041e431efa7de7f1a1cad88822b5a924f3530
ldj <tovin4613@gmail.com>
2023-12-28 11:17:30 +0900

    게시판 목록 api 데이터 출력

--- components/component/leedongjun3.tsx
+++ components/component/leedongjun3.tsx
@@ -4,32 +4,17 @@ import axios from 'axios';
 import { Button } from "@/components/ui/button";
 
 const Leedongjun3 = ({ bookId, setSelectedCompoId }) => {
-    const [book, setBook] = useState(null);
-    const [currentPageIndex, setCurrentPageIndex] = useState(0);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        if (bookId) {
-            axios.get(`http://127.0.0.1:8000/api/BookList/${bookId}`)
-                .then(response => {
-                    setBook(response.data);
-                })
-                .catch(error => {
-                    console.error('Error fetching book data:', error);
-                });
-        }
-    }, [bookId]);
-
-    const goToNextPage = () => {
-        if (currentPageIndex < book.BookDetail.length - 1) {
-            setCurrentPageIndex(currentPageIndex + 1);
-        }
-    };
-
-    const goToPreviousPage = () => {
-        if (currentPageIndex > 0) {
-            setCurrentPageIndex(currentPageIndex - 1);
-        }
-    };
+        axios.get(`http://127.0.0.1:8000/api/posts/`)
+            .then(response => {
+                setPosts(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching post data:', error);
+            });
+    }, []);
 
     return (
         <div className="container mx-auto px-4">
@@ -62,14 +47,14 @@ const Leedongjun3 = ({ bookId, setSelectedCompoId }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="cursor-pointer hover:bg-gray-50">
-                            <td className="border-b border-gray-200 p-4 text-center">1</td>
-                            <td className="border-b border-gray-200 p-4 text-center">
-                                <a href="/post/1" className="text-blue-600 hover:text-blue-800">게시물 제목</a>
-                            </td>
-                            <td className="border-b border-gray-200 p-4 text-center">홍길동</td>
-                            <td className="border-b border-gray-200 p-4 text-center">2023-12-27</td>
-                        </tr>
+                        {posts.map(post => (
+                            <tr key={post.id} className="cursor-pointer hover:bg-gray-50">
+                                <td className="border-b border-gray-200 p-4 text-center">{post.id}</td>
+                                <td className="border-b border-gray-200 p-4 text-center">{post.title}</td>
+                                <td className="border-b border-gray-200 p-4 text-center">{post.User.name}</td>
+                                <td className="border-b border-gray-200 p-4 text-center">{post.created_at}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
