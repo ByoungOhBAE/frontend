5da40a0d4729d3212780362c810b2f49d7de570c
inha <qaqrs99@gmail.com>
2023-12-28 11:34:42 +0900

    forum,inhaforum-게시판글상세조회,게시판글수정

--- components/component/forum.tsx
+++ components/component/forum.tsx
@@ -9,41 +9,54 @@ const Forum = ({ bookId, setSelectedCompoId }) => {
 
 
     return (
-        <div>
-        <div className='buttons' align='left'>
-            <input type='button' value={'메뉴'} style={{borderColor:'black',borderStyle:'solid',borderWidth:'1px'}}></input>
-        </div>
-        <div className='Title' align='center'>
-            <br></br>
-            <div className='post_title' align='center'>글 제목이 들어갈 자리입니다</div>
-            <div><br></br></div>
-            <div className='post_content' align='center'>본문 즉 질문이 들어갈 자리입니다ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ</div>
-        </div>
-        <div className='comment'>
-            <br></br>
-            <div className='comment' align='left' style={{marginLeft:150}}>댓글</div>
-            <div><br></br></div>
-            <div className='comment_content' align='center'>
-                <table>
-                    <col width='300'></col><col width='1950'></col><col width='300' ></col>
-                    <tr>
-                    <th>댓글작성자1</th><td>댓글 내용 1번--------------------------------------------------------------------------------------------------</td><td style={{marginLeft:200}}>작성일자YYYY.MM.DD</td>
-                    </tr>
-                    <tr>
-                    <th>댓글작성자2</th><td>댓글 내용 2번</td><td style={{marginLeft:200}}>작성일자YYYY.MM.DD</td>
-                    </tr>
-                    <tr>
-                    <th>댓글작성자3</th><td>댓글 내용 3번</td><td style={{marginLeft:200}}>작성일자YYYY.MM.DD</td>
-                    </tr>
-                </table>
-            </div>
-            <div><br></br></div>
-            <div className='write_comment' align='right'>
-                <input type='button' value='댓글 작성' style={{marginRight:200, borderColor:'black',borderStyle:'solid',borderWidth:'1px'}}></input>
+        <div className="container mx-auto px-4">
+        {/* 뒤로가기 버튼 */}
+        <button className="bg-white-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mb-4">
+            <img src='https://kr.seaicons.com/wp-content/uploads/2015/11/Arrows-Undo-icon.png' className='w-8 h-8'/>
+            
+        </button>
+
+        {/* 게시물 내용 */}
+        <div className="bg-white p-4 rounded-lg shadow-md mb-6">
+            <h1 className="text-3xl font-bold mb-4">게시물 제목</h1>
+            <p className="text-gray-700 mb-2">작성자: 홍길동</p>
+            <p className="text-gray-700 mb-4">작성 시간: 2023-12-27</p>
+            <div className="text-gray-800">
+                {/* 게시물 본문 내용 */}
+                본문 즉 질문이 들어갈 자리입니다...
             </div>
         </div>
+
+        {/* 댓글 테이블 */}
+        <div className="bg-white p-4 rounded-lg shadow-md">
+            <table className="min-w-full">
+                <thead className="bg-gray-100">
+                    <tr>
+                        <th className="border-b-2 border-gray-200 p-4 text-center">댓글 작성자</th>
+                        <th className="border-b-2 border-gray-200 p-4 text-center">댓글 내용</th>
+                        <th className="border-b-2 border-gray-200 p-4 text-center">작성일자</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="cursor-pointer hover:bg-gray-50">
+                        <td className="border-b border-gray-200 p-4 text-center">댓글작성자1</td>
+                        <td className="border-b border-gray-200 p-4 text-center">댓글 내용 1번---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</td>
+                        <td className="border-b border-gray-200 p-4 text-center">작성일자YYYY.MM.DD</td>
+                    </tr>
+                    {/* 추가적인 댓글 행들을 필요에 따라 반복해서 추가 */}
+                </tbody>
+            </table>
         </div>
-    );
+
+        {/* 댓글 작성하기 버튼 */}
+        <div className="text-right my-6">
+            <button className="bg-Stone-50 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+                <img src='https://cdn.icon-icons.com/icons2/2066/PNG/512/comments_icon_125320.png' className='w-8 h-8' />
+                
+            </button>
+        </div>
+    </div>
+);
 };
 
 export default Forum;
--- components/component/inhaforum.tsx
+++ components/component/inhaforum.tsx
@@ -1,35 +1,76 @@
-// components/BookDetailComponent.js 또는 .tsx
-import React, { useEffect, useState } from 'react';
-
-const Inhaforum = ({}) => {
+// components/WriteForm.js 또는 .tsx
+import React, { useState } from 'react';
+import { Button } from "@/components/ui/button";
+ 
+const InhaForum = ({ onCancel }) => {
+    const [postTitle, setPostTitle] = useState("");
+    const [postContent, setPostContent] = useState("");
+ 
+    const handlePostSubmit = () => {
+        // 글 작성 로직 추가
+        // 예: 서버에 데이터 전송
+ 
+        // 작성이 완료된 후 폼 닫기
+        onCancel();
+    };
     
-
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        setFile(selectedFile);
+    }
     return (
-        
-                <div class="search" align="left">
-                    <input type="text" placeholder="검색어 입력" style={{marginLeft: 650}}></input>
-                    <input type="button" value="검색" style={{marginLeft:30, borderColor:'black',borderStyle:'solid',borderWidth:'1px'}}></input>
-                    <input type="button" value="작성하기" style={{marginLeft: 950, borderColor:'black',borderStyle:'solid',borderWidth:'1px',}}></input>
-                <div><br></br><br></br></div>
-            <div align="center">    
-            <table border="1">
-                <col width="200"></col><col width="600"></col><col width="300"></col><col width="300"></col>
-                <tr>
-                    <th>번호</th><th>제목</th><th>작성자</th><th>작성일자</th>
-                </tr>
-                <tr>
-                    <th>3</th><td align="center">동화에 대해 질문이 있습니다</td><td align='center'>1번작성자</td><td align='center'>YYYY.MM.DD</td>
-                </tr>
-                <tr>
-                    <th>2</th><td align="center">동화 퀴즈를 못 풀겠어요</td><td align='center'>2번작성자</td><td align='center'>YYYY.MM.DD</td>
-                </tr>
-                <tr>
-                    <th>1</th><td align="center">책 줄거리가 이해가 안되요</td><td align='center'>3번작성자</td><td align='center'>YYYY.MM.DD</td>
-                </tr>
-            </table>
-            </div>
-            </div>
+        <div>
+            <h2 className="text-2xl font-bold mb-4">글 수정하기</h2>
+            <form onSubmit={handlePostSubmit}>
+                {/* 제목 입력 */}
+                <div className="mb-4">
+                    <label htmlFor="postTitle" className="block text-sm font-bold mb-2">제목</label>
+                    <input
+                        id="postTitle"
+                        type="text"
+                        value={postTitle}
+                        onChange={(e) => setPostTitle(e.target.value)}
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                        required
+                    />
+                </div>
+                {/* 내용 입력 */}
+                <div className="mb-4">
+                    <label htmlFor="postContent" className="block text-sm font-bold mb-2">내용</label>
+                    <textarea
+                        id="postContent"
+                        value={postContent}
+                        onChange={(e) => setPostContent(e.target.value)}
+                        className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                        rows="4"
+                        required
+                    ></textarea>
+                </div>
+                <div className='mb-4'>
+                    <label htmlFor='fileInput' className='block text-sm font-bold text-gray-700 mb-2'>
+                        첨부파일
+                    </label>
+                    <input
+                        type="file"
+                        id="fileInput"
+                        name="fileInput"
+                        className="p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 w-full"
+                        onChange={handleFileChange}
+                    />
+                </div>
+                {/* 작성 완료 버튼 */}
+                <div className='flex justify-end'>
+                <Button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+                >
+                    수정하기
+                </Button>
+                </div>
+            </form>
+        </div>
     );
 };
-
-export default Inhaforum;
+ 
+export default InhaForum;
+ 
