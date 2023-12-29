fe61f63549914a4f5bc9ed4a42f92b30dad29a88
pilt <sirver6469@gmail.com>
2023-12-29 17:19:39 +0900

    merge

--- components/component/book_list.tsx
+++ components/component/book_list.tsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Avatar } from "@/components/ui/avatar";
@@ -11,7 +9,7 @@ import { useSearchParams } from 'next/navigation';
 const PER_PAGE = 8;
 
 export default function Book_list({ setSelecteCompoId, setSelectedBookId }) {
-  
+
 
   const PER_PAGE = 8;
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,58 +21,55 @@ export default function Book_list({ setSelecteCompoId, setSelectedBookId }) {
   };
   const currentBooks = bookList ? bookList.slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE) : [];
   const totalPages = bookList ? Math.ceil(bookList.length / PER_PAGE) : 0;
-
-
-  
   return (
-      <div>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          {/* 책 목록 렌더링 */}
-          {currentBooks.map(book => (
-   
-            <div key={book.id}
-   
-              className="relative group overflow-hidden rounded-lg"
-              onClick={() => showBookDetails(book.id)}>
-   
-              {/* 책 정보 렌더링 */}
-              <Card style={{
-                backgroundImage: 'url(https://edu.chosun.com/site/data/img_dir/2011/01/24/2011012401165_0.jpg)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                minHeight: '250px'
-              }}>
-                <CardHeader>
-                  <Avatar className="w-12 h-12" src="/placeholder.svg?height=100&width=100" />
-                </CardHeader>
-                <CardContent>
-                  {/* 여기에 추가 정보 표시 */}
-                </CardContent>
-              </Card>
-   
-              {/* 밑은 호버 띄우기 */}
-              <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
-                onClick={() =>
-                  showBookDetails(book.id)
-                }>
+    <div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* 책 목록 렌더링 */}
+        {currentBooks.map(book => (
+
+          <div key={book.id}
+
+            className="relative group overflow-hidden rounded-lg"
+            onClick={() => showBookDetails(book.id)}>
+
+            {/* 책 정보 렌더링 */}
+            <Card style={{
+              backgroundImage: 'url(https://edu.chosun.com/site/data/img_dir/2011/01/24/2011012401165_0.jpg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              minHeight: '250px'
+            }}>
+              <CardHeader>
+                <Avatar className="w-12 h-12" src="/placeholder.svg?height=100&width=100" />
+              </CardHeader>
+              <CardContent>
+                {/* 여기에 추가 정보 표시 */}
+              </CardContent>
+            </Card>
+
+            {/* 밑은 호버 띄우기 */}
+            <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity"
+              onClick={() =>
+                showBookDetails(book.id)
+              }>
               <h2 className="text-white ml-4 text-lg font-semibold">
                 {book.book_name}
               </h2>
               <p className="text-white text-lg">바로가기</p>
             </div>
-            </div>
-        ))}
-        </div >
-        {/* 페이지네이션 버튼 */}
-        <div style={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: '30px'}}>
-            <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>이전 페이지</Button>
-            <span> {currentPage} / {totalPages}</span>
-            <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>다음 페이지</Button>
           </div>
+        ))}
+      </div >
+      {/* 페이지네이션 버튼 */}
+      <div style={{ display: 'flex', justifyContent: 'center', width: '100%', marginTop: '30px' }}>
+        <Button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>이전 페이지</Button>
+        <span> {currentPage} / {totalPages}</span>
+        <Button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>다음 페이지</Button>
       </div>
-    )
-  }
+    </div>
+  )
+}
 // 북리스트 가져오기 위한 인터페이스
 
 interface BookListComponentProps {
